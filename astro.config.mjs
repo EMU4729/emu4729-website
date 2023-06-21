3f89e94f2c06725e83ad9e667b4cb9e8789ed74d
Neel Sharma <nsdj.sharma@gmail.com>
2023-06-21 20:08:14 +1000

    minor seo + accessibility stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://emu4729-website.vercel.app", // TODO: Change to new domain once deployed
   integrations: [
     image(),
     tailwind({
