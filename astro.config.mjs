96c093ca5bfa9c65abcda7db62f368873c82faeb
Neel Sharma <nsdj.sharma@gmail.com>
2025-01-23 12:45:12 +1100

    upgrade tailwindcss to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://first.unsw.edu.au", // TODO: Change to new domain once deployed
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap(),
-  ],
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
